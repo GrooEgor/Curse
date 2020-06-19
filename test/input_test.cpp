8978727b41136d2fd45ee2300a54cfa14e107a10
S1deWay <eliseev1999@inbox.ru>
2020-06-19 17:00:09 +0700

    Update makefile and test

---
+++ test/input_test.cpp
@@ -0,0 +1,6 @@
+#include <catch.hpp>
+#include <input.h>
+TEST_CASE("#1", "[1]")
+{
+    REQUIRE(1 == 1);
+}
---
+++ test/output_test.cpp
@@ -0,0 +1,7 @@
+#define CATCH_CONFIG_MAIN
+#include <catch.hpp>
+#include <output.h>
+TEST_CASE("#2", "[2]")
+{
+    REQUIRE(1 == 1);
+}
