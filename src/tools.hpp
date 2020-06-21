c84ebf187436e51fd750e9f309ebf1d9442f06fc
Lane-x <lanexman28@gmail.com>
2020-06-21 21:40:53 +0700

    update delete punctuation marks test, add new cases

--- src/tools.hpp
+++ src/tools.hpp
@@ -4,7 +4,6 @@
 #include <vector>
 
 void Delete_punctuation_marks(std::string& str);
-bool Check_permissible_symbols(const char symbol);
 bool Check_permissible_word(const std::string str);
 void Quick_Sort(std::vector<std::string>& arr, int first, int last);
 
--- test/tools_test.cpp
+++ test/tools_test.cpp
@@ -4,7 +4,7 @@
 
 using namespace std;
 
-TEST_CASE("Delete_punctuation_marks", "[input]")
+TEST_CASE("Delete_punctuation_marks", "[tools]")
 {
     GIVEN("String with punctuations marks")
     {
@@ -15,6 +15,32 @@ TEST_CASE("Delete_punctuation_marks", "[input]")
             THEN("Marks will be deleted")
             {
                 string expected = "with marks";
+                REQUIRE(given == expected);
+            }
+        }
+    }
+    GIVEN("String without punctuations marks")
+    {
+        string given = "without marks";
+        WHEN("Using delete function")
+        {
+            Delete_punctuation_marks(given);
+            THEN("String will not change")
+            {
+                string expected = "without marks";
+                REQUIRE(given == expected);
+            }
+        }
+    }
+    GIVEN("Empty string")
+    {
+        string given = "";
+        WHEN("Using delete function")
+        {
+            Delete_punctuation_marks(given);
+            THEN("String will not change")
+            {
+                string expected = "";
                 REQUIRE(given == expected);
             }
         }
