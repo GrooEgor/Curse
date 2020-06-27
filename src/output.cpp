15d3ea846a40af6a1afcfe050e8e61e00eebffc0
S1deWay <eliseev1999@inbox.ru>
2020-06-27 23:07:35 +0700

    Added save to file function

--- src/output.cpp
+++ src/output.cpp
@@ -12,3 +12,14 @@ void Print_Words(vector<string> document_data, string path)
         cout << document_data[i] << endl;
     }
 }
+void Save_to_file(vector<string> document_data, string path, int number)
+{
+    cout << "Сreating a dictionary file on example - " << path << endl;
+    ofstream out("./dictionaries/" + to_string(number) + ".txt", ios::app);
+    if (out.is_open()) {
+        for (size_t i = 0; i < document_data.size(); i++) {
+            out << document_data[i] << endl;
+        }
+    }
+    out.close();
+}
